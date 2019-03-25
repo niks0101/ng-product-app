528aa100afa91b0e211d57febe36ed83bf257b7d
niks0101 <niks9289.bomble@gmail.com>
2019-03-25 07:35:53 +0530

    first commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,25 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { AuthGuard } from './_guards';
+import { ProductListComponent } from '@/product-list/product-list.component';
+import { ProductDetailComponent } from '@/product-detail/product-detail.component';
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    { path: "products", component: ProductListComponent },
+    { path: "product/:id", component: ProductDetailComponent },
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
